495e225ba6d4c7b04de1c2616aa12ec6d6897a91
Giancarlo <giancarlo.mateus@outlook.com>
2023-04-23 19:10:23 -0300

    lab 2 fixes

--- Lab2/blinky.c
+++ Lab2/blinky.c
@@ -23,17 +23,16 @@ int main(void)
 {
   // LAB2 CODE
   bool gameRunnning = false;
+  bool gameStarted = false;
   unsigned int gameStartTime = 1000;
   unsigned int gameEndTime = gameStartTime + 3000;
-  unsigned int gameStartTicks = 0;
+  unsigned int totalTime = 0;
   // END LAB2 CODE
 
   // estado de cada led
-  bool Led1 = false, Led2 = false, Led3 = false, Led4 = false;
+  bool Led1 = false, Led4 = false;
   // controle via programa��o de anti-debouncing
   bool bt1flag = false, bt2flag = false;
-  // controle de tempo para cada led
-  unsigned int led1time = 0, led2time = 0;
   // controle de tempo para cada bot�o
   unsigned int bt1time = 0, bt2time = 0;
   // Configura o clock para utilizar o xtal externo de 25MHz
@@ -70,141 +69,102 @@ int main(void)
   // loop infinito
   while (1)
   {
-    // descomente o printf para entender os valores lidos dos bot�es ... modo debug com termial I/O
-    // printf("Port J: Pin_0: [%d] - Pin_1: [%d]\n",GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_0),GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_1));
-    // controle temporal do led1 - tempo 1s = 1000ms
-    //  if (SysTicks1ms>=led1time)
-    //     {
-    //       //tempo do led recebe o tempo atual + o tempo futuro para entrar novamente no if
-    //       led1time=SysTicks1ms+1000;
-    //       //altera o estado do led ligado / desligado
-    //       Led1=!Led1;
-    //       //escreve o estado do led no pino
-    //       GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1<<1);
-    //     }
-    // controle temporal do led2 - tempo 0,5s = 500ms
-
-    // LAB2 CODE
-
-    if (!gameRunnning)
+    //debounce botão 1
+    if (bt1flag)
     {
-      if (SysTicks1ms >= gameStartTime)
-      {
-        printf("\nJOGO INICIADO \n");
-        gameRunnning = true;
-        Led1 = true;
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1 << 1);
-        gameStartTicks = SysTickValueGet();
-        gameEndTime = gameStartTime + 3000;          
-        printf("Tempo inicio: %d\n", gameStartTicks);
-
-      }
-    }
-    else
-    {
-      if(SysTicks1ms >= gameEndTime){
-        printf("GAME OVER\n");
-        // altera o estado do led ligado / desligado
-          Led1 = false;
-          // escreve o estado do led no pino
-          GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1 << 1);
-          gameRunnning = false;
-          gameStartTime = SysTicks1ms + 1000;
-      }
-      else if (bt1flag)
-      {
-        // Bot�o1 liberado !!!
-        if (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) && SysTicks1ms >= bt1time)
-        {
-          // bot�o liberado
-          bt1flag = false;
-          // 55ms para liberar estado do bot�o ... tempo anti-debouncing
-          bt1time = SysTicks1ms + 55;
-        }
-      }
-      else
-      {
-        // bot�o1 pressionado !!!
-        if ((GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) == 0) && (SysTicks1ms >= bt1time))
-        {
-          // bot�o pressionado
-          bt1flag = true;
-          // 55ms para liberar estado do bot�o ... tempo anti-debouncing
-          bt1time = SysTicks1ms + 55;
-          // altera o estado do led ligado / desligado
-          Led1 = false;
-          // escreve o estado do led no pino
-          GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1 << 1);
-          gameRunnning = false;
-          gameStartTime = SysTicks1ms + 1000;
-          printf("Tempo fim: %d\n", SysTickValueGet());
-          printf("Tempo total: %d\n",SysTickValueGet() - gameStartTicks);
-        }
-      }
-    }
-    // END LAB2 CODE
-
-//    if (SysTicks1ms >= led2time)
-//    {
-//      // tempo do led recebe o tempo atual + o tempo futuro para entrar novamente no if
-//      led2time = SysTicks1ms + 500;
-//      // altera o estado do led ligado / desligado
-//      Led2 = !Led2;
-//      // escreve o estado do led no pino
-//      GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, Led2);
-//    }
-    // ant-debouncig do bot�o 1
-    // if (bt1flag)
-    // {
-    //   // Bot�o1 liberado !!!
-    //   if (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) && SysTicks1ms >= bt1time)
-    //   {
-    //     // bot�o liberado
-    //     bt1flag = false;
-    //     // 55ms para liberar estado do bot�o ... tempo anti-debouncing
-    //     bt1time = SysTicks1ms + 55;
-    //   }
-    // }
-    // else
-    // {
-    //   // bot�o1 pressionado !!!
-    //   if ((GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) == 0) && (SysTicks1ms >= bt1time))
-    //   {
-    //     // bot�o pressionado
-    //     bt1flag = true;
-    //     // 55ms para liberar estado do bot�o ... tempo anti-debouncing
-    //     bt1time = SysTicks1ms + 55;
-    //     // altera o estado do led ligado / desligado
-    //     Led3 = !Led3;
-    //     // escreve o estado do led no pino
-    //     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, Led3 << 4);
-    //   }
-    // }
-    // ant-debouncig do bot�o 2
-    if (bt2flag)
-    {
-      // bot�o2 liberado !!!
-      if (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) && (SysTicks1ms >= bt2time))
+      // Bot�o1 liberado !!!
+      if (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) && SysTicks1ms >= bt1time)
       {
         // bot�o liberado
-        bt2flag = false;
+        bt1flag = false;
         // 55ms para liberar estado do bot�o ... tempo anti-debouncing
-        bt2time = SysTicks1ms + 55;
+        bt1time = SysTicks1ms + 55;
       }
     }
     else
     {
-      // bot�o2 pressionado !!!
-      if ((GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) == 0) && (SysTicks1ms >= bt2time))
+      // bot�o1 pressionado !!!
+      if ((GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) == 0) && (SysTicks1ms >= bt1time))
       {
         // bot�o pressionado
-        bt2flag = true;
+        bt1flag = true;
         // 55ms para liberar estado do bot�o ... tempo anti-debouncing
-        bt2time = SysTicks1ms + 55;
+        bt1time = SysTicks1ms + 55;
         // altera o estado do led ligado / desligado
-        Led4 = !Led4;
+        Led1 = false;
+        // escreve o estado do led no pino
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1 << 1);
+        gameRunnning = false;
+        gameStarted = true;
+        Led4 = false;
         // escreve o estado do led no pino
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, Led4);
+        printf("\nJOGO INICIADO \n");
+        
+        gameStartTime = SysTicks1ms + 1000;
+      }
+      //Dá start no game caso o botão 1 já tenha sido pressionado
+      else if (gameStarted)
+      {
+        if (!gameRunnning)
+        {
+          if (SysTicks1ms >= gameStartTime)
+          {
+            gameRunnning = true;
+            Led1 = true;
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1 << 1);
+            gameStartTime = SysTicks1ms;
+            gameEndTime = gameStartTime + 3000;
+          }
+        }
+        else
+        {
+          if (SysTicks1ms >= gameEndTime)
+          {
+            printf("GAME OVER\n");
+            // altera o estado do led ligado / desligado
+            Led1 = false;
+            // escreve o estado do led no pino
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1 << 1);
+            Led4 = true;
+            // escreve o estado do led no pino
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, Led4);
+            gameRunnning = false;
+            gameStarted = false;
+          }
+          //debounce botão 2
+          else if (bt2flag) 
+          {
+            // bot�o2 liberado !!!
+            if (GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) && (SysTicks1ms >= bt2time))
+            {
+              // bot�o liberado
+              bt2flag = false;
+              // 55ms para liberar estado do bot�o ... tempo anti-debouncing
+              bt2time = SysTicks1ms + 55;
+            }
+          }
+          else
+          {
+            // bot�o2 pressionado !!!
+            if ((GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) == 0) && (SysTicks1ms >= bt2time))
+            {
+              // bot�o pressionado
+              bt2flag = true;
+              // 55ms para liberar estado do bot�o ... tempo anti-debouncing
+              bt2time = SysTicks1ms + 55;
+              // altera o estado do led ligado / desligado
+              Led1 = false;
+              // escreve o estado do led no pino
+              GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, Led1 << 1);
+              totalTime = SysTicks1ms - gameStartTime;
+              gameRunnning = false;
+              gameStarted = false;
+
+              printf("Tempo total: %dms\n", totalTime);
+            }
+          }
+        }
       }
     }
   }
